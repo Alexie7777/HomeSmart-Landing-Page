272623eb5d671b64c3339416910d55a993934aad
Alex7777 <934956752@qq.com>
2022-07-07 16:37:31 +0800

    🆕 change HomeSmart Logo to theme toggler

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 const navBtn = document.getElementById("menu");
 const menuBar = document.querySelector('[role="menubar"]');
+const darkToggle = document.getElementById("theme-toggle");
 
 navBtn.addEventListener("click", () => {
   // const isExpanded =
@@ -9,4 +10,17 @@ navBtn.addEventListener("click", () => {
   navBtn.setAttribute("aria-expanded", !isExpanded);
   menuBar.classList.toggle("hidden");
   menuBar.classList.toggle("flex");
+});
+
+darkToggle.addEventListener("click", () => {
+  const theme = localStorage.getItem("color-theme");
+  const toggleDarkMode = document.documentElement.classList.toggle("dark");
+  if (theme == "dark") {
+    toggleDarkMode;
+    localStorage.setItem("color-theme", "light");
+  }
+  if (theme == "light") {
+    toggleDarkMode;
+    localStorage.setItem("color-theme", "dark");
+  }
 });
